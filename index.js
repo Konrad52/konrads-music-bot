80a60077a3e7822d94e75f206539f0e4e13a46c7
BuildTools <unconfigured@null.spigotmc.org>
2020-11-09 20:38:52 +0100

    Finished the main thingies idk.

--- index.js
+++ index.js
@@ -1,62 +1,88 @@
+const getYoutubeTitle = require('get-youtube-title')
+const getYoutubeId = require('get-youtube-id');
 const discord = require('discord.js');
+const request = require('request');
 const ytdl = require('ytdl-core');
-const util = require('util');
+const fs = require('fs');
+
 const client = new discord.Client();
 
-var prefix = ".";
+require('dotenv').config();
 
+var prefix = ".";
 var servers = {};
+
+function isYoutube(string) {
+    return string.toLowerCase().indexOf("youtube.com") > -1;
+}
+
+function getID(string, callback) {
+    if (isYoutube(string)) {
+        callback(getYoutubeId(string));
+    } else {
+        searchYoutube(string, function (id) {
+            callback(id);
+        });
+    }
+}
+
+function dispatcherEnd(server, message) {
+    if (server.queue[0]) {
+        setTimeout(() => {play(server.connection, message)}, 20000);
+    } else {
+        server.current = '';
+        setTimeout(() => {server.connection.disconnect()}, 20000);
+    }
+}
+
+function dispatcherEndNoTimeout(server, message) {
+    if (server.queue[0]) {
+        play(server.connection, message);
+    } else {
+        server.current = '';
+        server.connection.disconnect();
+    }
+}
+
+function play(connection, message) {
+    const server = servers[message.guild.id];
+    
+    server.current = server.queue[0];
+    server.queue.splice(0, 1);
+    server.connection = connection;
+    
+    let ytdlInstance = ytdl("https://www.youtube.com/watch?v=" + server.current, {quality: 'highestaudio', filter: 'audioonly', highWaterMark: 1 << 25});
+    message.channel.send('Currently playing: "' + server.current + '"');
+
+    ytdlInstance.on('end', () => {
+        dispatcherEnd(server, message);
+    });
+
+    connection.play(ytdlInstance);
+    server.dispatcher = ytdlInstance;
+}
+
+function searchYoutube(query, callback) {
+    request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + process.env.YT_API_KEY, function(error, response) {
+        if (error) throw error;
+        callback(JSON.parse(response.body).items[0].id.videoId);
+    });
+}
+
+function queue(queueString, server, id, callback) {
+    if (id < server.queue.length)
+        getYoutubeTitle(server.queue[id], function(err, title) {
+            if (err) throw err;
+            queueString += ' - ' (id + 1) + ': "' + title + '"\n';
+            id++;
+            queue(queueString, server, id, callback);
+        });
+    else callback(queueString);
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
-function play(connection, server, message) {
-    server.current = server.queue[0];
-    server.queue.splice(0, 1);
-    
-    server.ytdlInstance = ytdl(server.current, {quality: 'highestaudio', filter: 'audioonly', highWaterMark: 1 << 25});
-    server.ytdlInstance.on('end', () => {
-        if (server.queue[0]) {
-            server.ytdlInstance = undefined;
-            play(connection, server, message);
-        } else {
-            connection.disconnect();
-            server.current = '';
-        }
-    });
-
-    connection.play(server.ytdlInstance);
-}
-
-const queue = async function(server) {
-    let queueString = '';
-    let id = 1;
-    if (server.queue.length > 0) {
-        ytdl.getBasicInfo(server.current).then(currentInfo => {
-            queueString += 'Currently playing: ' + currentInfo.title + '\n';
-            server.queue.forEach(song => {
-                ytdl.getBasicInfo(song).then(info => {
-                    queueString += id.toString() + '. ' + info.title + '\n';
-                    id++;
-                });
-            });  
-        });           
-    } else {
-        if (server.current == '')
-            queueString = '- No songs on queue.\n'
-        else {
-            ytdl.getBasicInfo(server.current).then(currentInfo => {
-                queueString += 'Currently playing: ' + currentInfo.title + '\n';
-            });
-        }
-    }
-    return queueString;
-}
-
-function callQueue(server) {
-    return queue(server);
-}
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix))
@@ -66,7 +92,8 @@ client.on('message', message => {
         servers[message.guild.id] = {
             queue: [],
             current: '',
-            ytdlInstance: null
+            dispatcher: null,
+            connection: null
         }
     }
     const server = servers[message.guild.id];
@@ -77,58 +104,103 @@ client.on('message', message => {
     const args = split.slice(1, split.length);
 
     switch (command) {
+        case 'p':
         case 'play':
             if (!message.member.voice.channel) {
                 message.channel.send('You must be in a voice channel to use this command!');
                 return;
             }
-
             if (args.length < 1) {
                 message.channel.send('You must specify what to play!');
                 return;
             }
-
-            if (args.length > 1) {
-                message.channel.send('You must ONLY specify what to play!');
-                return;
-            }
         
-            if (!message.guild.voice || !message.guild.voice.channel) {
-                message.member.voice.channel.join().then((connection) => {
-                    server.queue.push(args[0]);
-                    connection.voice.setSelfDeaf(true);
-                    play(connection, server, message);
-                });
+            let search = '';
+            if (!isYoutube(args[0]))
+                search = message.content.substring(prefix.length + command.length + 1);
+            else
+                search = args[0];
+            
+            getID(search, function(id) {
+                server.queue.push(id);
+
+                if (!message.guild.voice || !message.guild.voice.channel) {
+                    message.member.voice.channel.join().then((connection) => {
+                        connection.voice.setSelfDeaf(true);
+                        play(connection, message);
+                    });
+                } else {
+                    if (message.member.voice && message.member.voice.channelID == message.guild.voice.channelID)
+                        message.channel.send('Song added to queue: "' + id + '"\nTo get the current queue type: `.queue`.');
+                    else
+                        message.channel.send('I\'m already in another voice channel!');
+                    return;
+                }
+            });
+
+            break;
+
+        case 's':
+        case 'skip':
+            if (server.dispatcher)
+                dispatcherEndNoTimeout(server, message);
+            break;
+
+        case 'r':
+        case 'remove':
+            if (args.length > 0) {
+                if (args.length != 1) {
+                    message.channel.send('Too many arguments!');
+                    return;
+                }
             } else {
-                if (message.member.voice && message.member.voice.channelID == message.guild.voice.channelID) {
-                    server.queue.push(args[0]);
-                    message.channel.send('Song added to queue! To get the current queue type: `.queue`.');
-                } else
-                    message.channel.send('I\'m already in a voice channel!');
+                message.channel.send('You need to specify the song\'s number that you want to remove.');
                 return;
             }
 
+            if (isNaN(args[0])) {
+                message.channel.send('You need to specify the song\'s number that you want to remove.');
+                return;
+            }
+            
+            if (parseInt(args[0]) > server.queue.length + 1 && parseInt(args[0]) > 0) {
+                message.channel.send('Invalid song number!');
+                return;
+            }
+
+            getYoutubeTitle(server.queue[parseInt(args[0]) - 1], function(err, title) {
+                if (err) throw err;
+                message.channel.send('The song "' + title + '" has been removed from the queue.');
+            });
+            server.queue.splice(parseInt(args[0]) - 1, 1);
             break;
 
-        case 'skip':
-            server.ytdlInstance.end();
-            break;
-
+        case 'fuckoff':
+        case 'leave':
         case 'stop':
             server.current = '';
             server.queue = [];
-            server.ytdlInstance.end();
-            //if (message.guild.voice && message.guild.voice.connection) message.guild.voice.connection.disconnect();
+            if (server.dispatcher)
+                dispatcherEndNoTimeout(server, message);
             break;
 
+        case 'q':
         case 'queue':
-            (async() => {
-                queue(server).then((queueString) => {
-                    message.channel.send(
-                        '```python\n' + queueString + '```'
-                    );
+            let queueString = '';
+
+            if (server.current == '') {
+                message.channel.send('```python\n - No songs on queue\n```');
+                return;
+            }
+
+            let id = 0;
+            getYoutubeTitle(server.current, function(err, title) {
+                if (err) throw err;
+                queueString += ' - Currently playing: "' + title + '"\n';
+                queue(queueString, server, id, (finalString) => {
+                    message.channel.send('```python\n' + finalString + '```');
                 });
-            })();
+            });
             break;
     }
 });
